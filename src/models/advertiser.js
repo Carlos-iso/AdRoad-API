7e261901d098df202c28bdc0e2002dc42ec7fa80
Carlos-iso <carlosbalak2002@gmail.com>
2025-04-09 00:12:43 -0300

    PC

--- src/models/advertiser.js
+++ src/models/advertiser.js
@@ -1,41 +1,59 @@
-'use strict';
+"use strict";
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const { isLowercase } = require("validator");
 const Schema = mongoose.Schema;
 // const validator = require('mongoose-validator');
 
 const schema = new Schema({
-    name_enterprise: {
-        type: String,
-        required: [true, 'O Nome É Necessário'],
-        unique: false
-    },
-    email: {
-        type: String,
-        required: [true, 'O E-mail É Necessário'],
-        unique: true
-    },
-    cnpj: {
+  name_enterprise: {
+    type: String,
+    required: [true, "O Nome É Necessário"],
+    unique: false,
+  },
+  email: {
+    type: String,
+    required: [true, "O E-mail É Necessário"],
+    unique: true,
+  },
+  cnpj: {
+    type: String,
+    required: [true, "O CNPJ É Necessário"],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "A Senha É Necessário"],
+    unique: false,
+  },
+  balance_ad: {
+    type: Number,
+    decimal: true,
+    required: true,
+    default: 0,
+  },
+  adress: {
+    street: { type: String, required: true, trim: true, required: [true, "Necessário Informar Rua"] },
+    number: { type: String, required: true, default: "S/N" },
+    complement: { type: String, trim: true },
+    neighborhood: { type: String, required: true, trim: true },
+    city: { type: String, required: [true, "Necessário Informar Cidade"], trim: true },
+    state: {
       type: String,
-      required:[true, 'O CNPJ É Necessário'],
-      unique: true
+      trim: true,
+      required: [true, "Necessário Informar Estado"],
     },
-    password: {
-        type: String,
-        required: [true, 'A Senha É Necessário'],
-        unique: false
+    postalCode: {
+      type: String,
+      required: [true, "Necessário Informar CEP"],
+      match: [/^\d{5}-?\d{3}$/, "CEP inválido"],
     },
-    createDate: {
-        type: Date,
-        required: [true, 'A Data É Necessário'],
-        default: Date.now
-    },
-    balance_ad: {
-      type: Number,
-      decimal: true,
-      require: true,
-      default: 0
-    }
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
 });
 
-module.exports = mongoose.model('Advertiser', schema);
+module.exports = mongoose.model("Advertiser", schema);
