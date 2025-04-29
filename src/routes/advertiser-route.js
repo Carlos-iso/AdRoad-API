ab97b8040c96a29f8e7032f31cd98e765f201150
Carlos Siqueira <93467076+Carlos-iso@users.noreply.github.com>
2025-04-29 01:01:23 -0300

    Atualizar o advertiser-route.js
    Rápida alteração na roda de id

--- src/routes/advertiser-route.js
+++ src/routes/advertiser-route.js
@@ -6,7 +6,7 @@ const controller = require("../controllers/advertiser-controller.js");
 const authService = require("../services/auth-service.js");
 
 router.get("/", controller.get);
-router.get("/:id", controller.getById);
+router.get("/:id", authService.authorize, controller.getById);
 router.post("/new", controller.post);
 router.post("/login", controller.authenticate);
 router.post("/refresh-token", authService.authorize, controller.refreshToken);
